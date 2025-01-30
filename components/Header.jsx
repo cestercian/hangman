23f8bd83551a1dabc2fa682e4cb27e63eb8ee5b4
Cestercian <yashafaid@gmail.com>
2025-01-31 02:17:52 +0530

    v.1.0

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return(
+        <>
+            <h1>Assembly: Endgame</h1>
+            <h2>Guess the word in under 8 attempts
+                to keep the programming world safe from Assembly!
+            </h2>
+        </>
+       )
+}

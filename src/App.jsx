20d8045f91e8d1eb8430df4ec53b02b4d6359457
Cestercian <yashafaid@gmail.com>
2025-02-06 14:42:50 +0530

    v.1.0

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import Header from "../components/Header.jsx";
 
 function App(){
 
-
-
-
     return (
-        <Header/>
+         <>
+             <header>
+                 <h1>Assembly: Endgame</h1>
+                 <p>Guess the word within 8 attempts to keep the
+                     programming world safe from Assembly!</p>
+             </header>
+         </>
     )
 }
 

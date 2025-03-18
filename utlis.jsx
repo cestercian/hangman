07f3dfa515501c3a70d88ae12e9b1786becad5f8
Cestercian <yashafaid@gmail.com>
2025-03-19 02:35:49 +0530

    v.2.0

--- utlis.jsx
+++ utlis.jsx
@@ -1,17 +1,16 @@
 export default function getFarewellText(language) {
     const options = [
-        `Farewell, ${language}`,
-        `Adios, ${language}`,
-        `R.I.P., ${language}`,
-        `We'll miss you, ${language}`,
-        `Oh no, not ${language}!`,
-        `${language} bites the dust`,
-        `Gone but not forgotten, ${language}`,
-        `The end of ${language} as we know it`,
-        `Off into the sunset, ${language}`,
-        `${language}, it's been real`,
-        `${language}, your watch has ended`,
-        `${language} has left the building`
+        `Farewell, ${language} 😢`,
+        `Adios, ${language} 👋`,
+        `R.I.P., ${language} 💀`,
+        `We'll miss you, ${language} 😭`,
+        `Oh no, not ${language}! 😱`,
+        `${language} bites the dust 💀`,
+        `Gone but not forgotten, ${language} 😔`,
+        `The end of ${language} as we know it 😬`,
+        `${language}, it's been real 😞`,
+        `${language}, your watch has ended 💀`,
+        `${language} has left the building 😕`
     ];
 
     const randomIndex = Math.floor(Math.random() * options.length);

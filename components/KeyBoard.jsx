8613514775485a9b3d0fdc3054c7418c8b637f3e
Cestercian <yashafaid@gmail.com>
2025-03-19 07:55:07 +0530

    v.3.0

--- components/KeyBoard.jsx
+++ components/KeyBoard.jsx
@@ -1,6 +1,6 @@
 export default function KeyBoard(prop){
     return(
-        <button onClick={prop.insert} className={prop.className}>
+        <button disabled={prop.isDisable} onClick={prop.insert} className={prop.className}>
             {prop.name}
         </button>
     )
